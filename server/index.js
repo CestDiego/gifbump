e32ba28241d82895beab055ccbab75baef9cdbbe
Barak Chamo <barakchamo@gmail.com>
2015-10-21 14:55:35 -0400

    specify openshify ip for server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-http.createServer(onRequest).listen(process.env.OPENSHIFT_NODEJS_PORT || 3000);
+http.createServer(onRequest).listen(process.env.OPENSHIFT_NODEJS_PORT || 3000, process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 
 function onRequest(client_req, client_res) {
   console.log('serve: ' + client_req.url);
