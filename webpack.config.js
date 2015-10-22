95898b8ac1a80945f8e769b658a90b531c0ff650
Barak Chamo <barakchamo@gmail.com>
2015-10-22 16:16:15 -0400

    initial landing page

--- webpack.config.js
+++ webpack.config.js
@@ -23,12 +23,49 @@ module.exports = [
 	                exclude: /(node_modules|bower_components)/,
 	                loader: 'babel?optional[]=runtime'
 	            },
-	            // // CSS loader
-	            // {
-	            //     test: /\.css$/,
-	            //     loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-	            // }
-	            // CSS loader
+	            {
+	                test: /\.scss$/,
+	                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
+	            },
+	            {
+	                test: /\.woff2$/,
+	                loader: 'file'
+	            }
+	        ]
+	    },
+	    resolve: {
+	        modulesDirectories: [
+	            'node_modules',
+	            'resources',
+	            'app'
+	        ],
+	        extensions: ['.js', '.json', '.jsx', '']
+	    },
+	    plugins: [
+	        new ExtractTextPlugin('styles.css')
+	    ]
+	},
+
+	{
+		name: 'website',
+
+	    entry: {
+	        app:     ['./app/website/index.jsx']
+	    },
+
+	    output: {
+	        path: path.resolve(__dirname, 'dist', 'extension'),
+	        publicPath: '/',
+	        filename: '[name].js'
+	    },
+	    module: {
+	        loaders: [
+	            // Babel loader
+	            {
+	                test: /\.jsx?$/,
+	                exclude: /(node_modules|bower_components)/,
+	                loader: 'babel?optional[]=runtime'
+	            },
 	            {
 	                test: /\.scss$/,
 	                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
