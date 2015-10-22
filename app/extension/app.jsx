ca70ae2c23717a1afeee24e38ff2015f55a2a4dc
Diego Berrocal <cestdiego@gmail.com>
2015-10-22 16:07:48 -0400

    missed app.jsx

---
+++ app/extension/app.jsx
@@ -0,0 +1,12 @@
+import { GIF } from 'gif.js'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// Components
+import Index from './components/Index'
+
+// Render app container
+ReactDOM.render(
+  <Index/>,
+  document.getElementById('container')
+)
